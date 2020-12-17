c1547dd9d53d6619349f2bff09cdbe432bac1f73
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-17 12:28:43 +0200

    add color box

--- class_06/lab_06.js
+++ class_06/lab_06.js
@@ -1,8 +1,8 @@
-// var WlcMassage = "Welcome in the world of information"
-// alert(WlcMassage);
-// var yourName = prompt("What is you name?")
-// alert ('hello ' + yourName);
-// confirm('Are you interested in PDF books')
+var WlcMassage = "Welcome in the world of information"
+alert(WlcMassage);
+var yourName = prompt("What is you name?")
+alert ('hello ' + yourName);
+confirm('Are you interested in PDF books')
 function display()
          {
              var col=document.getElementById("demo");
