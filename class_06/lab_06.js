7ade873785ed24e0900b932401773a9fcdfe24a0
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-17 13:49:54 +0200

    edit lab_06

--- class_06/lab_06.js
+++ class_06/lab_06.js
@@ -1,11 +1,15 @@
 var WlcMassage = "Welcome in the world of information"
 alert(WlcMassage);
-var yourName = prompt("What is you name?")
-alert ('hello ' + yourName);
-confirm('Are you interested in PDF books')
+var yourName = 'A name'
+if (yourName = prompt('What is your name?')){
+    alert ('hello ' + yourName);confirm('Are you interested in PDF books')}
+    else {alert('it is not a name')}
+
+
 function display()
          {
              var col=document.getElementById("demo");
              var color=document.getElementById("inputCol");
              col.style.color=color.value;
          }
+          
