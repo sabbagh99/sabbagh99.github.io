c85d37fc8442783137c00e7c7d5d75f0a167e593
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-17 08:57:22 +0200

    add lab_06

---
+++ class_06/lab_06.js
@@ -0,0 +1,5 @@
+var WlcMassage = "Welcome in the world of information"
+alert(WlcMassage);
+var yourName = prompt("What is you name?")
+alert ('hello ' + yourName);
+confirm('Are you interested in PDF books')
