d9b56b2ebcdbb13e6c4bc39ae6ff493ae3b4cb42
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2020-12-27 11:01:29 +0200

    fix

---
+++ class_06/lab_06.js
@@ -0,0 +1,15 @@
+var WlcMassage = "Welcome in the world of information"
+alert(WlcMassage);
+var yourName = 'A name'
+if (yourName = prompt('What is your name?')){
+    alert ('hello ' + yourName);confirm('Are you interested in PDF books')}
+    else {alert('it is not a name')}
+
+
+function display()
+         {
+             var col=document.getElementById("demo");
+             var color=document.getElementById("inputCol");
+             col.style.color=color.value;
+         }
+          
